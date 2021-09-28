d6b0f8b2cf19280a11c1d03cadc5ab9833eef6fb
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-09-28 13:20:19 +0300

    complex

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-int main() {}
+#include <iostream>
+using namespace std;
+
+#include "complex.hpp"
+
+int main() {
+    // complex test
+    Complex a(1, 5), b(4, 8);
+    cout << a + b << '\n';
+    cout << a * b << '\n';
+    cout << a / b;
+}
