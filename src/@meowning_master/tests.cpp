21ccafa0488f04a73053dd27a14953a0146cca38
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-11-09 12:52:58 +0200

    complex test

--- src/@meowning_master/tests.cpp
+++ src/@meowning_master/tests.cpp
@@ -82,24 +82,45 @@ void test_measure_duration() {
 }
 
 void test_inverse_by_minors() {
-     Matrix<Complex> m = {
+    Matrix<Complex> m;
+    Matrix<Complex> r;
+    const int repeat_time = 1000;
+    nanoseconds ns_duration;
+    double op_duration;
+
+    m = {
         {{1, 0}, {0, 0}, {3, 0}},
         {{4, 0}, {5, 0}, {6, 0}},
         {{7, 0}, {8, 0}, {9, 0}}
     };
-    Matrix<Complex> r;
-    const int repeat_time = 1000;
-    auto ns_duration = measure_duration([&](){
+    ns_duration = measure_duration([&](){
         for (int i = 0; i < repeat_time; i += 1) {
             r = matrix::inverse_by_minors(m);
         }
     });
-    auto op_duration = static_cast<double>(duration_cast<milliseconds>(ns_duration).count()) / repeat_time;
+    op_duration = static_cast<double>(duration_cast<milliseconds>(ns_duration).count()) / repeat_time;
     Matrix<Complex> c = {
         {{1./4, 0}, {-2, 0}, {5./4, 0}},
         {{-1./2, 0}, {1, 0}, {-1./2, 0}},
         {{1./4, 0}, {2./3, 0}, {-5./12, 0}}
     };
     assert(cmp_matrixes(r, c));
-    cout << "Inverse by minors duration:  " << op_duration << "ms" << '\n';
+    cout << "Inverse by minors 1 duration:  " << op_duration << "ms" << '\n';
+
+    m = {
+        {{2, 3}, {-5, 0}},
+        {{14, 0}, {0, 1}},
+    };
+    ns_duration = measure_duration([&](){
+        for (int i = 0; i < repeat_time; i += 1) {
+            r = matrix::inverse_by_minors(m);
+        }
+    });
+    op_duration = static_cast<double>(duration_cast<milliseconds>(ns_duration).count()) / repeat_time;
+    c = {
+        {{0.000445137, 0.0149121}, {0.0745604, -0.00222568}},
+        {{-0.208769, 0.00623192}, {0.0311596, 0.043846}}
+    };
+    assert(cmp_matrixes(r, c));
+    cout << "Inverse by minors 2 duration:  " << op_duration << "ms" << '\n';
 }
